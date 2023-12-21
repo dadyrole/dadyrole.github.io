aa7bbebf8a38f7c7adfb322994202396e47fbe76
dadyrole <62624025+dadyrole@users.noreply.github.com>
2023-12-21 09:41:26 +0300

    revolution

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
-function animation() {
-  const elements = document.getElementsByClassName('typing-animation');
-
-  for (let i = 0; i < elements.length; i++) {
-      main(elements[i]);
-  }
-
-  function main(el) {
-      if (!el) {
-          console.log("el = null\nReturning . . .");
-          return;
-      } else {
-          console.log(`animating: ${el.innerText}`);
-      }
-
-      const replacement = document.createElement('div');
-      replacement.classList.add('flex-row-center');
-      const newspan = document.createElement('span');
-      newspan.classList.add('blinking-caret');
-      replacement.appendChild(el.cloneNode(true));
-      replacement.appendChild(newspan);
-      el.parentNode.replaceChild(replacement, el);
-
-      el = replacement.firstChild;
-      const text = el.innerText;
-      const interval = 150; // ms
-      let i = 0;
-
-      function appendText() {
-          if (i < text.length) {
-              el.innerText = text.substring(0, ++i);
-              setTimeout(appendText, interval);
-          }
-      }
-      function blink() {
-          if (newspan.style.visibility === 'visible') {
-            newspan.style.visibility = 'hidden';
-          } else {
-            newspan.style.visibility = 'visible';
-          }
-      }
-
-      el.innerText = '';
-      setTimeout(appendText, interval);
-      setInterval(blink, interval*3);
-  }
+const text = (name) => {
+    return `Добро пожаловать в ${name} – удивительное место, где встречаются традиции и современность. Этот регион славится своей богатой историей, захватывающими природными красотами и уникальной культурой. Здесь каждый уголок пропитан аутентичным колоритом, а местные достопримечательности не оставят вас равнодушными. Откройте для себя удивительные места, насладитесь гостеприимством местных жителей и создайте свои неповторимые воспоминания в ${name}.`
 }
 
-window.addEventListener('load', animation);
+function turnVisibility(element){
+    if(element.classList.contains('display-none')){
+        element.classList.remove('display-none');
+    }else{
+        element.classList.add('display-none');
+    }
+}
+
+function transit(){
+    const Svg = document.getElementById('map-svg');
+    const contentBox = document.getElementById('content');
+    turnVisibility(Svg);
+    turnVisibility(contentBox);
+    turnVisibility(closeButton);
+}
+
+
+function toContent(district){
+    const svgContent = document.getElementById('svg-content');
+
+    const pathBounds = district.getBBox();
+
+    transit();
+    
+    if(svgContent.childNodes.length > 0){
+        svgContent.childNodes.forEach( (child) => { svgContent.removeChild(child) } );
+    }
+    svgContent.appendChild(district.cloneNode(true));
+    svgContent.setAttribute('viewBox', `${pathBounds.x} ${pathBounds.y} ${pathBounds.width} ${pathBounds.height}`);
+    
+    const paragraph = document.getElementById('text-content')
+    paragraph.textContent = text(district.getAttribute('data-title'));
+}
+
+const districtList = document.querySelectorAll('[data-code]');
+districtList.forEach((district) => {
+    district.addEventListener('mouseenter', () => {
+        const Selected = document.getElementById('map-selected');
+        Selected.textContent = district.getAttribute('data-title');
+    });
+
+    district.addEventListener('click', (event) => {
+        if (event.button !== 0) return;
+        toContent(district);
+    });
+});
+
+const closeButton = document.getElementById('close-button');
+closeButton.addEventListener('click', (event) => {
+    if(event.button !== 0) return;
+    transit();
+})
